e5107a811913ebd12648ce3af9649445ec7e80d4
Santiago Lacki <santiagolacki@gmail.com>
2025-04-16 20:42:01 -0300

    Se agrego un ejemplo con Do-While en el archivo de bucles

--- Js/04-Bucles.js
+++ Js/04-Bucles.js
@@ -1,10 +1,11 @@
 // Bucles = Estructuras de repeticion
 // while - do while - for
 
-
 /* 
 
-  Mientras se cumple la condicion logica
+  -- Mientras se cumple la condicion logica --
+
+  - WHILE -
 
   while(condicion logica) {
     (Lineas de codigo que quiero repetir)
@@ -15,7 +16,29 @@
 
 let renglon = 1;
 
-while (renglon <= 50) {
+while (renglon <= 10) {
   document.writeln(`<p> Renglon numero ${renglon} </p>`);
   renglon = renglon + 1;
 }
+
+/* 
+
+  -- Hacer mientras se cumple la condicion logica --
+
+  - DO WHILE -
+
+  do {
+    (Lineas de codigo que quiero repetir)
+    Agregar una linea que haga que la condicion logica se deje de cumplir
+  }
+  while (Condicion logica)
+
+*/
+
+let linea = 1;
+
+do {
+  document.writeln(`<p> Linea numero: ${linea} </p>`);
+  linea = linea + 1
+}
+while (linea<=10)
